3bf5aa6431ccf0185785a5c30169d65b61d4da6e
Daniel García García <fillito@gmail.com>
2014-10-31 21:24:20 +0100

    Double transformer tests

---
+++ Demo/PDKTModelBuilderTests/PDKTDataTransformers/DoubleTransformerTests.m
@@ -0,0 +1,38 @@
+//
+//  DoubleTransformerTests.m
+//  PDKTModelBuilder
+//
+//  Created by Daniel García on 31/10/14.
+//  Copyright (c) 2014 Produkt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "PDKTDoubleTransformer.h"
+
+@interface DoubleTransformerTests : XCTestCase
+@property (strong,nonatomic) PDKTDoubleTransformer *doubleTransformer;
+@end
+
+@implementation DoubleTransformerTests
+
+- (void)setUp {
+    [super setUp];
+    self.doubleTransformer = [PDKTDoubleTransformer new];
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testReturnsADouble {
+    XCTAssert([[self.doubleTransformer parsedValueForObject:@"0.5"] isKindOfClass:NSClassFromString(@"__NSCFNumber")]);
+}
+
+- (void)testDoubleFromString {
+    NSNumber *number = [self.doubleTransformer parsedValueForObject:@"0.5"];
+    XCTAssertEqual([number floatValue], 0.5);
+}
+
+@end
