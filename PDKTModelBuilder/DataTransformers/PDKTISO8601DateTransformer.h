8c3c93499083e213ecec3aa4cdc8efac4f275ec2
Sergio Fernández <fdz.sergio@gmail.com>
2016-03-01 13:24:32 +0100

    added new ISO8601 date transformer with milliseconds accuracy

---
+++ PDKTModelBuilder/DataTransformers/PDKTISO8601DateTransformer.h
@@ -0,0 +1,13 @@
+//
+//  PDKTISO8601DateTransformer.h
+//  PDKTModelBuilder
+//
+//  Created by sergio on 01/03/16.
+//  Copyright © 2016 Produkt. All rights reserved.
+//
+
+#import "PDKTDataTransformer.h"
+
+@interface PDKTISO8601DateTransformer : PDKTDataTransformer
+
+@end
---
+++ PDKTModelBuilder/DataTransformers/PDKTISO8601DateTransformer.m
@@ -0,0 +1,39 @@
+//
+//  PDKTISO8601DateTransformer.m
+//  PDKTModelBuilder
+//
+//  Created by sergio on 01/03/16.
+//  Copyright © 2016 Produkt. All rights reserved.
+//
+
+#import "PDKTISO8601DateTransformer.h"
+
+@implementation PDKTISO8601DateTransformer
+
+- (id)tranformValueFromObject:(id)object {
+    // Returns a NSDate object that corresponds to the specified RFC 3339 date time string.
+    
+    if (!object || [object isEqual:[NSNull null]]) {
+        return nil;
+    }
+    NSString *objectDescription = [NSString stringWithFormat:@"%@", object];
+    NSDate *date = nil;
+    NSCharacterSet *alphaNumbersSet = [NSCharacterSet decimalDigitCharacterSet];
+    NSCharacterSet *stringSet = [NSCharacterSet characterSetWithCharactersInString:objectDescription];
+    if ([alphaNumbersSet isSupersetOfSet:stringSet]) {
+        date = [NSDate dateWithTimeIntervalSince1970:[objectDescription integerValue]];
+    } else {
+        static dispatch_once_t onceToken;
+        static NSDateFormatter *entityPropertyDateFormatter;
+        dispatch_once(&onceToken, ^{
+            entityPropertyDateFormatter = [[NSDateFormatter alloc] init];
+            entityPropertyDateFormatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+            entityPropertyDateFormatter.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
+            [entityPropertyDateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
+        });
+        date = [entityPropertyDateFormatter dateFromString:objectDescription];
+    }
+    return date;
+}
+
+@end
